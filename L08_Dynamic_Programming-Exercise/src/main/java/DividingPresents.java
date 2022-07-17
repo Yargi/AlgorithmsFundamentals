08b08ea64ecc0efa8106a2638ae8c32f7732a1cf
Markus Krusch <markus@kruschi.de>
2022-07-17 15:45:29 +0200

    added solution 'dividing presents'

--- L08_Dynamic_Programming-Exercise/src/main/java/DividingPresents.java
+++ L08_Dynamic_Programming-Exercise/src/main/java/DividingPresents.java
@@ -6,76 +6,55 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DividingPresents {
-
-    private static int[] gifts;
-    private static List<Integer> alansGifts = new ArrayList<>();
-    private static int sumAlan = 0;
-    private static List<Integer> bobsGifts = new ArrayList<>();
-    private static int sumBob = 0;
-    private static int fiftyFifty;
-    private static int difference = 0;
 
     public static void main(String[] args) throws IOException {
 
         // read input from console
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-        gifts = Arrays.stream(rd.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int[] gifts = Arrays.stream(rd.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
         rd.close();
+
         // declare variables
-        fiftyFifty = Arrays.stream(gifts).sum() / 2;
-        // calculations
-        divideGifts();
+        int totalSum = IntStream.of(gifts).sum();
+        int fiftyFifty = totalSum / 2;
 
+        // memoization range (sum of presents + 1)
+        int[] sumIndexer = new int[totalSum + 1];
+        Arrays.fill(sumIndexer, -1);
+        sumIndexer[0] = 0;
 
+        // iteration (gifts size * memo size)
+        for (int i = 0; i < gifts.length; i++) {
+
+            int giftValue = gifts[i];
+            for (int prevGiftIndex = totalSum - gifts[i]; prevGiftIndex >= 0; prevGiftIndex--) {
+                // check for unset value in memo
+                if (sumIndexer[prevGiftIndex] != -1 && sumIndexer[prevGiftIndex + giftValue] == -1) {
+                    // set the index in memo
+                    sumIndexer[prevGiftIndex + giftValue] = i;
+                }
+            }
+        }
+        // get the last set index from down half of memo
+        while (sumIndexer[fiftyFifty] == -1) {
+            fiftyFifty--;
+        }
 
         // build result string
         StringBuilder out = new StringBuilder();
-        out.append("Difference: ").append(difference).append(System.lineSeparator());
-        out.append("Alan:").append(sumAlan).append(" Bob:").append(sumBob).append(System.lineSeparator());
-        out.append("Alan takes: ").append(alansGifts.stream().map(String::valueOf).collect(Collectors.joining(" "))).append(System.lineSeparator());
-        out.append("Bob takes the rest.").append(System.lineSeparator());
-        // print
+        out.append("Difference: ").append(totalSum - fiftyFifty * 2).append(System.lineSeparator());
+        out.append("Alan:").append(fiftyFifty).append(" Bob:").append(totalSum - fiftyFifty).append(System.lineSeparator());
+        out.append("Alan takes:");
+        // iterate through memo by following previous index and append gift value to output string
+        while (fiftyFifty > 0) {
+            out.append(" ").append(gifts[sumIndexer[fiftyFifty]]);
+            fiftyFifty -= gifts[sumIndexer[fiftyFifty]];
+        }
+        out.append(System.lineSeparator()).append("Bob takes the rest.").append(System.lineSeparator());
+        // print to console
         System.out.print(out);
     }
-
-    private static void divideGifts() {
-
-        for (int i = 0; i < gifts.length; i++) {
-            int a = gifts[i];
-            if (sumBob <= sumAlan) {
-                bobsGifts.add(a);
-                sumBob += a;
-            } else {
-                alansGifts.add(a);
-                sumAlan += a;
-            }
-        }
-
-        difference = Math.abs(sumBob - sumAlan) ;
-
-        while (!(sumAlan == fiftyFifty)
-                && (bobsGifts.stream().anyMatch(g -> g <= difference)
-                || alansGifts.stream().anyMatch(g -> g <= difference))) {
-
-            int a = 0;
-            if (sumBob <= sumAlan) {
-                a = alansGifts.stream().filter(g -> g <= difference).findAny().get();
-                alansGifts.remove(alansGifts.indexOf(a));
-                bobsGifts.add(a);
-                sumAlan -= a;
-                sumBob += a;
-            } else {
-                a = bobsGifts.stream().filter(g -> g <= difference).findAny().get();
-                bobsGifts.remove(bobsGifts.indexOf(a));
-                alansGifts.add(a);
-                sumBob -= a;
-                sumAlan += a;
-            }
-
-            difference = Math.abs(sumBob - sumAlan);
-        }
-    }
-
 }
