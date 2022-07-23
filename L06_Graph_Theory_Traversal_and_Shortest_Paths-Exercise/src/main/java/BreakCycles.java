ea6a25d0be7de9b6f30abad77c40f18162409189
Yargi <markus.krusch@gmail.com>
2022-07-23 12:43:55 +0200

    added Graph Theory homework

--- L06_Graph_Theory_Traversal_and_Shortest_Paths-Exercise/src/main/java/BreakCycles.java
+++ L06_Graph_Theory_Traversal_and_Shortest_Paths-Exercise/src/main/java/BreakCycles.java
@@ -6,93 +6,146 @@ import java.util.stream.Collectors;
 
 public class BreakCycles {
 
-    private static Map<String, Set<String>> g = new TreeMap<>();
+    private static Map<String, Set<String>> graph = new HashMap<>();
     private static List<String> removedEdges = new ArrayList<>();
-
-    private static boolean acyclic = true;
+    private static SortedSet<String> outputCycle;
 
     public static void main(String[] args) throws IOException {
 
-        StringBuilder out = new StringBuilder();
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+
         String line = rd.readLine();
-        while (line != null && !line.trim().equals("")) {
 
-            String[] token = line.split(" -> ");
+        while (line != null && !line.equals("")) {
 
-            String key = token[0];
-            g.putIfAbsent(key, new TreeSet<>());
+            String[] node = line.split(" -> ");
+            String[] edgeParts = node.length > 1 ? node[1].split(" ") : new String[0];
+            Set<String> temp = new HashSet<>();
 
-            List<String> edges = token.length > 1 ? Arrays.stream(token[1].split("\\s+")).collect(Collectors.toList()) : new ArrayList<>();
-            for (String edge : edges) {
-                boolean duplicate = !g.get(key).add(edge);
-                if (duplicate && !removedEdges.contains(String.format("%s - %s", edge, key))) {
-                    removedEdges.add(String.format("%s - %s", key, edge));
+            for (String edge : edgeParts) {
+
+                boolean success = temp.add(edge.trim());
+                if (!success) {
+
+                    if (node[0].compareTo(edge) > 0) {
+                        removedEdges.add(String.format("%s - %s", edge, node[0]));
+                    }
                 }
             }
-
+            graph.put(node[0], temp);
             line = rd.readLine();
         }
+
         rd.close();
 
-        Set<String> visited = new HashSet<>();
-        for (String key : g.keySet()) {
+        while (true) {
 
-        checkForCycles(key, visited, new HashSet<>(), null);
+            Set<String> visited = new HashSet<>();
+            boolean noCycles = true;
 
-        while (!acyclic) {
+            for (String node : graph.keySet()) {
 
-                breakCycle();
+                SortedSet<String> cycle = new TreeSet<>();
+                outputCycle = new TreeSet<>();
+                boolean cyclic = searchForCycles(node, visited, cycle, null);
+
+                if (cyclic) {
+
+                    noCycles = false;
+                    breakCycle();
+                    break;
+                }
+            }
+            if (noCycles) {
+                break;
             }
         }
 
 
-
-
-
-        out.append("Edges to remove: ").append(removedEdges.size()).append(System.lineSeparator());
-        out.append(String.join(System.lineSeparator(), removedEdges));
-
-        System.out.println(out);
+        System.out.println("Edges to remove: " + removedEdges.size());
+        removedEdges = removedEdges.stream().sorted().collect(Collectors.toList());
+        for (String edge : removedEdges) {
+            System.out.println(edge);
+        }
     }
 
     private static void breakCycle() {
 
-        for (String node : g.keySet()) {
+        Map<String, Integer> vertexEdges = new HashMap<>();
 
-            Set<String> edges = g.get(node);
+        for (String node : outputCycle) {
 
-            for (String edge : edges) {
-                g.get(node).remove(edge);
-                acyclic = true;
-                checkForCycles(edge, new HashSet<>(), new HashSet<>(), null);
-                if (!acyclic) {
-                    g.get(node).add(edge);
-                } else {
-                    removedEdges.add(String.format("%s - %s", node, edge));
+            for (String vertex : outputCycle) {
+
+                if (graph.get(node).contains(vertex)) {
+
+                    vertexEdges.putIfAbsent(node, 0);
+                    vertexEdges.put(node, vertexEdges.get(node) + 1);
+                }
+            }
+        }
+
+        while (true) {
+
+            boolean hasChanged = false;
+
+            for (String node : vertexEdges.keySet()) {
+
+                if (vertexEdges.get(node) == 1 && outputCycle.contains(node)) {
+
+                    outputCycle.remove(node);
+                    hasChanged = true;
+                    break;
+                }
+            }
+            if (!hasChanged) {
+
+                break;
+            }
+        }
+
+        for (String node : outputCycle) {
+
+            for (String vertex : outputCycle) {
+
+                if (graph.get(node).contains(vertex)) {
+
+                    removedEdges.add(String.format("%s - %s", node, vertex));
+                    graph.get(node).remove(vertex);
+                    graph.get(vertex).remove(node);
+                    return;
                 }
             }
         }
     }
 
-    private static void checkForCycles(String node, Set<String> visited, Set<String> cycle, String lastNode) {
+    private static boolean searchForCycles(String node, Set<String> visited, SortedSet<String> cycle, String parent) {
+
+        boolean output = false;
 
         if (cycle.contains(node)) {
-            acyclic = true;
-            return;
+
+            outputCycle.addAll(cycle);
+            return true;
         }
+
         if (!visited.contains(node)) {
+
             visited.add(node);
             cycle.add(node);
 
-            for (String child : g.get(node)) {
-                if (!child.equals(lastNode)) {
-                    checkForCycles(child, visited, cycle, node);
-                }
-            }
-            cycle.remove(node);
-        }
-        return;
-    }
+            if (graph.containsKey(node)) {
 
+                for (String child : graph.get(node)) {
+
+                    if (!child.equals(parent)) {
+
+                        output = output || searchForCycles(child, visited, cycle, node);
+                    }
+                }
+                cycle.remove(node);
+            }
+        }
+        return output;
+    }
 }
